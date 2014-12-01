30f936517d51f8a0a559aa80e1b5720b88cfe833
BlueEyedHush <blueeyedhush@gmail.com>
2014-12-01 11:27:03 +0100

    - created client app skeleton - added networking properties to config

---
+++ client/src/main/scala/notemaker/client/Application.scala
@@ -0,0 +1,10 @@
+package notemaker.client
+
+/**
+ * Created by blueeyedhush on 12/1/14.
+ */
+object Application {
+  def main(args : Array[String]) = {
+    Configuration.load()
+  }
+}
---
+++ client/src/main/scala/notemaker/client/ServerCommunication.scala
@@ -0,0 +1,8 @@
+package notemaker.client
+
+/**
+ * Created by blueeyedhush on 12/1/14.
+ */
+class ServerConnection {
+
+}
