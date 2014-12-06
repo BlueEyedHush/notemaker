c0412cb0afcf2981f1f6968f6e1459becb07bb55
BlueEyedHush <blueeyedhush@gmail.com>
2014-12-06 08:56:00 +0100

    Initial GUI code.

--- client/src/main/scala/notemaker/client/Application.scala
+++ client/src/main/scala/notemaker/client/Application.scala
@@ -6,17 +6,41 @@ package notemaker.client
 
 import java.util.logging.Logger
 
-object Application {
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.control.ScrollPane
+import scalafx.scene.layout.StackPane
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
+
+object Application extends JFXApp {
   val logger : java.util.logging.Logger = Logger.getLogger("Global")
 
-  def main(args : Array[String]) = {
-    logger.info("Application started")
-    Configuration.load()
-    SessionManager.connect(Configuration.config.getString("networking.serverip"),
-      Configuration.config.getInt("networking.port"))
+  logger.info("Application started")
+  Configuration.load()
+  SessionManager.connect(Configuration.config.getString("networking.serverip"),
+    Configuration.config.getInt("networking.port"))
 
-    NodeManager.createNode(new Node(1445, -4673))
+  NodeManager.createNode(new Node(1445, -4673))
 
-    SessionManager.disconnect()
+  stage = new PrimaryStage {
+    title = "Simple ScalaFX App"
+    scene = new Scene() {
+      root = new StackPane {
+        content = new ScrollPane() {
+          padding = Insets(20)
+          pannable = true
+          content = new Rectangle() {
+            width = 200
+            height = 200
+            fill = Color.DEEPSKYBLUE
+          }
+        }
+      }
+    }
   }
+
+  SessionManager.disconnect()
 }
