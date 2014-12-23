3215a35fa9d246c267145fbb22de1eacd1fb2fa5
BlueEyedHush <blueeyedhush@gmail.com>
2014-12-23 17:31:58 +0100

    [Client] GUI code refactored

---
+++ client/src/main/scala/notemaker/client/GuiElements.scala
@@ -0,0 +1,64 @@
+package notemaker.client
+
+import scalafx.Includes._
+import javafx.event
+import javafx.scene.input.MouseEvent
+import scalafx.scene.layout.Pane
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
+
+/**
+ * Created by blueeyedhush on 12/23/14.
+ */
+object JfxWorksheet extends Pane {
+  var sequence: Seq[Rectangle] = Seq()
+
+  def createNode(x1 : Double, x2 : Double) = {
+    val node = new JfxNode(x1, x2)
+
+    sequence = sequence :+ node
+    content.add(node)
+  }
+
+  onMouseClicked() = new event.EventHandler[MouseEvent] {
+    override def handle( event: MouseEvent ): Unit ={
+      if (event.getClickCount == 2) {
+        val x = event.getX
+        val y = event.getY
+        NodeManager.createNode(new Node(x.toInt, y.toInt))
+        createNode(x,y)
+        ()
+      }
+    }
+  }
+}
+
+class JfxNode(x1 : Double, y1 : Double) extends Rectangle {
+  width = 50
+  height = 50
+  x = x1.toInt
+  y = y1.toInt
+  fill = Color.WhiteSmoke
+  var tempX: Int = 0
+  var tempY: Int = 0
+  onMousePressed = new event.EventHandler[MouseEvent] {
+    // Rectangle focus handler
+    override def handle(event: MouseEvent): Unit = {
+      fill = Color.Black
+      tempX = event.getX.toInt - x.toInt
+      tempY = event.getY.toInt - y.toInt
+    }
+  }
+  onMouseReleased = new event.EventHandler[MouseEvent] {
+    override def handle(event: MouseEvent): Unit ={
+      fill = Color.WhiteSmoke
+    }
+  }
+  onMouseDragged = new event.EventHandler[MouseEvent] {
+    override def handle(event: MouseEvent): Unit = {
+      println(tempX.toString + " " + tempY.toString)
+      x = event.getX.toInt - tempX
+      y = event.getY.toInt - tempY
+    }
+  }
+}
--- client/src/main/scala/notemaker/client/NotemakerApp.scala
+++ client/src/main/scala/notemaker/client/NotemakerApp.scala
@@ -44,56 +44,7 @@ class NotemakerApp extends Application {
   @Override
   override def init() : Unit = {
     NotemakerApp.logger.info("Application started")
-
-    Configuration.load()
-    val serverip : String = Configuration.config.getString("networking.serverip")
-    val port : Int = Configuration.config.getInt("networking.port")
-
-    NetworkingService.connect(serverip, port)
-    NetworkingService.dispatchers().add(new LoggingDispatcher)
-    NodeManager.init()
-
-    // Old version - is this useless?
-    //    NodeManager.createNode(new Node(1445, -4673))
-    //    NodeManager.createNode(new Node(1445, -467))
   }
-
-
-  //this function is alfa
-  def createInfobox(x1 : Double, y1 : Double) : Rectangle = {
-    NodeManager.createNode(new Node(x1.toInt, y1.toInt))
-    val rect = new Rectangle(){
-      width = 50
-      height = 50
-      x = x1.toInt
-      y = y1.toInt
-      fill = Color.WhiteSmoke
-      var tempX: Int = 0
-      var tempY: Int = 0
-      onMousePressed = new event.EventHandler[MouseEvent] {
-        // Rectangle focus handler
-        override def handle(event: MouseEvent): Unit = {
-          fill = Color.Black
-          tempX = event.getX.toInt - x.toInt
-          tempY = event.getY.toInt - y.toInt
-        }
-      }
-      onMouseReleased = new event.EventHandler[MouseEvent] {
-        override def handle(event: MouseEvent): Unit ={
-          fill = Color.WhiteSmoke
-        }
-      }
-      onMouseDragged = new event.EventHandler[MouseEvent] {
-        override def handle(event: MouseEvent): Unit = {
-          println(tempX.toString + " " + tempY.toString)
-          x = event.getX.toInt - tempX
-          y = event.getY.toInt - tempY
-        }
-      }
-    }
-    rect
-  }
-
 
   @Override
   override def start(stage : javafx.stage.Stage) : Unit = {
@@ -101,31 +52,13 @@ class NotemakerApp extends Application {
     stage.setWidth(800)
     stage.setHeight(600)
 
-
-    var sequence: Seq[Rectangle] = Seq()
-
-    //Our inifinty sheet:
-    val scrollPane = new Pane{
-
-      onMouseClicked() = new event.EventHandler[MouseEvent] {
-        override def handle( event: MouseEvent ): Unit ={
-          if (event.getClickCount == 2) {
-            val newBox = createInfobox(event.getX(), event.getY())
-            sequence = sequence :+ newBox
-            newBox.onMouseClicked()
-            content.add(newBox)
-          }
-        }
-      }
-    }
-
     //Keyhandler - for testing reasons
     val scene = new Scene() {
-      root = scrollPane
+      root = JfxWorksheet
       onKeyPressed() = new event.EventHandler[KeyEvent] {
         override def handle(event: KeyEvent): Unit = {
           println(event.getCode.toString)
-          for(elem <- sequence: Seq[Rectangle]) print(elem.getX.toString())
+          for(elem <- JfxWorksheet.sequence: Seq[Rectangle]) print(elem.getX.toString())
         }
       }
       }
@@ -133,6 +66,23 @@ class NotemakerApp extends Application {
     stage.setScene(scene)
     stage.show()
 
+    NodeManager.nodeListener = (n : Node) => {
+        JfxWorksheet.createNode(n.x.toDouble, n.y.toDouble)
+        ()
+      }
+
+    initializeLogic()
+  }
+
+  protected def initializeLogic() : Unit = {
+    Configuration.load()
+    val serverip : String = Configuration.config.getString("networking.serverip")
+    val port : Int = Configuration.config.getInt("networking.port")
+
+    NetworkingService.connect(serverip, port)
+    NetworkingService.dispatchers().add(new LoggingDispatcher)
+    NodeManager.init()
+    ()
   }
 
   @Override
