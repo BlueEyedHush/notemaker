d4936d45f3992b00e8a4c22530a02eb628b7a18e
Jakub Synowiec <shnek94@gmail.com>
2014-12-23 16:56:07 +0100

    Minor update - cleaning code

---
+++ client/src/test/scala/ServerConnectionTest.scala
@@ -0,0 +1,34 @@
+/**
+ * Created by blueeyedhush on 12/1/14.
+ */
+
+import org.json4s.DefaultFormats
+import org.junit.{BeforeClass, Test, Assert}
+import notemaker.client._
+
+object ServerConnectionTest {
+  var scon : ServerConnection = null
+
+  @BeforeClass
+  def create() = {
+    Configuration.load()
+    scon = new ServerConnection(Configuration.config.getString("networking.serverip"),
+    Configuration.config.getInt("networking.port"))
+  }
+}
+
+class ServerConnectionTest {
+  @Test
+  def canClientCommunicateWithServer() = {
+    ServerConnectionTest.scon.send("{\"mtype\":\"Test\",\"content\":{}}")
+    Assert.assertEquals(ServerConnectionTest.scon.receiveWait(5000), "{\"mtype\":\"Test\",\"content\":{}}")
+  }
+
+  @Test
+  def isCaseClassToJsonConvWorking() = {
+    implicit val format = DefaultFormats
+    val msgObj = new GenericMessage(mtype = "NodeCreated", content = new NodeCreatedContent(x = 1445, y= -467))
+    val json = org.json4s.native.Serialization.write(msgObj)
+    Assert.assertEquals("{\"mtype\":\"NodeCreated\",\"content\":{\"x\":1445,\"y\":-467}}", json)
+  }
+}
---
+++ server/src/main/erlang/goodGod.erl
@@ -0,0 +1,89 @@
+%%%-------------------------------------------------------------------
+%%% @author blueeyedhush
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 01. Dec 2014 9:28 PM
+%%%-------------------------------------------------------------------
+-module(goodGod).
+-author("blueeyedhush").
+-include("../include/global.hrl").
+%% API
+-export([spawn/0, start/0, loop/1, inf_clientConn/0, inf_nodeCreated/1, inf_clientDisconn/0, req_content/0]).
+
+spawn() ->
+  Pid = erlang:spawn_link(?MODULE, start, []),
+  {ok, Pid}.
+
+start() ->
+  info_msg("[gG] Started"),
+  register(goodGod, self()),
+  process_flag(trap_exit, true),
+  {ok, Port} = application:get_env(port),
+  {ok, LS} = gen_tcp:listen(Port, [{active, true}, list]),
+  io:format("~w \n", [LS]),
+  %spawn first child
+  FirstChildPID = erlang:spawn(guardianAngel, start, [LS]),
+  loop(#state{listenSocket = LS, clientList = [FirstChildPID], nodeList = []}).
+
+loop(State) ->
+  receive
+    {clientConnected, PID} ->
+      info_msg("[gG] Client connected"),
+      erlang:spawn(guardianAngel, start, [State#state.listenSocket]),
+      goodGod:loop(State#state{clientList = [PID|State#state.clientList]});
+    {clientDisconnected, PID} ->
+      info_msg("[gG] Client disconnected"),
+      goodGod:loop(State#state{clientList = remove_from_list(PID, State#state.clientList, [])});
+    {nodeCreated, PID, Descriptor} ->
+      info_msg("[gG] New node created with coords: " ++ integer_to_list(Descriptor#nodeCreated.x) ++ " " ++ integer_to_list(Descriptor#nodeCreated.y)),
+      NewState = State#state{nodeList = [#node{posX = Descriptor#nodeCreated.x, posY = Descriptor#nodeCreated.y}|State#state.nodeList]},
+      broadcast_to_all_but(Descriptor, PID, State#state.clientList),
+      goodGod:loop(NewState);
+    {reqContent, PID} ->
+      send_content(PID, State),
+      goodGod:loop(State);
+    {'EXIT', _, _} ->
+      terminate(State);
+    A ->
+      info_msg("[gG] Unexpected message has arrived: ~p", [A]),
+      goodGod:loop(State)
+  end.
+
+terminate(State) ->
+  info_msg("[gG] Terminating..."),
+  gen_tcp:close(State#state.listenSocket).
+
+broadcast_to_all_but(_, _, []) -> ok;
+broadcast_to_all_but(Record, But, [R|Recipients]) when R == But ->
+  broadcast_to_all_but(Record, But, Recipients);
+broadcast_to_all_but(Record, But, [R|Recipients]) when R /= But ->
+  send_retransmit(R, Record),
+  broadcast_to_all_but(Record, But, Recipients).
+
+remove_from_list(El, [], Acc) ->
+  lists:reverse(Acc);
+remove_from_list(El, [A|List], Acc) when El == A ->
+  remove_from_list(El, List, Acc);
+remove_from_list(El, [A|List], Acc) ->
+  remove_from_list(El, List, [A|Acc]).
+
+% wrappers for message-based communication with goodGod
+inf_clientConn() ->
+  goodGod ! {clientConnected, self()}.
+
+inf_clientDisconn() ->
+  goodGod ! {clientDisconnected, self()}.
+
+inf_nodeCreated(Descriptor) ->
+  goodGod ! {nodeCreated, self(), Descriptor}.
+
+req_content() ->
+  goodGod ! {reqContent, self()}.
+
+send_retransmit(PID, Mesg) ->
+  PID ! {gG, retrans, Mesg}.
+
+send_content(PID, State) ->
+  PID ! {gG, content, State#state.nodeList}.
