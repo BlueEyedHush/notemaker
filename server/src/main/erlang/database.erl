b661bb4e8a5383bd5172e5f97b591c3bba2389dc
BlueEyedHush <blueeyedhush@gmail.com>
2015-01-15 08:12:53 +0100

    Database support added alongside the old mechanism.

--- server/src/main/erlang/database.erl
+++ server/src/main/erlang/database.erl
@@ -10,12 +10,16 @@
 -author("blueeyedhush").
 
 -include("../include/global.hrl").
+-include_lib("stdlib/include/ms_transform.hrl").
 
 %% API
--export([start/0, shutdown/0]).
+-export([start/0, shutdown/0, createNode/1, deleteNode/1, updateNode/2, getNodeById/1,
+  getAllNodes/0, getOrCreateFFID/1, updateFFID/1]).
+
+% Initialization
 
 start() ->
-  SchemeLoc = application:get_env(dbLoc),
+  {ok, SchemeLoc} = application:get_env(dbLoc),
   application:load(mnesia),
   application:set_env(mnesia, dir, SchemeLoc),
   case mnesia:create_schema([node()]) of
@@ -24,16 +28,94 @@ start() ->
       io:format("\n ~p \n", [Reason]);
     _ -> info_msg(<<"Schema successfully created">>)
   end,
-  application:start(mnesia),
+  application:ensure_started(mnesia),
   case mnesia:create_table(node, [{attributes, record_info(fields, node)}, {type, set}]) of
     {atomic, ok} -> info_msg(<<"Created node table">>);
-    {aborted, {already_exists, T}} -> info_msg(<<"Table node already exists.">>);
+    {aborted, {already_exists, _}} -> info_msg(<<"Table node already exists.">>);
     {aborted, Reason1} ->
-      error_msg(<<"Table creation failed!">>),
+      error_msg(<<"node table creation failed!">>),
       io:format("\n ~p \n", [Reason1]),
+      exit(normal)
+  end,
+  case mnesia:create_table(config, [{attributes, record_info(fields, config)}, {type, set}]) of
+    {atomic, ok} -> info_msg(<<"Created config table">>);
+    {aborted, {already_exists, _}} -> info_msg(<<"Table config already exists.">>);
+    {aborted, Reason2} ->
+      error_msg(<<"config table creation failed!">>),
+      io:format("\n ~p \n", [Reason2]),
       exit(normal)
   end,
   mnesia:wait_for_tables([node], 3000).
 
-shutdown() ->
-  application:stop(mnesia).
+shutdown() -> ok.
+  % @ToDo: For some reason, this takes veery long, causing supervisor to be killed
+  %application:stop(mnesia).
+
+% Data Manipulation
+createNode(Node) when is_record(Node, node) ->
+  mnesia:activity(async_dirty,
+    fun() ->
+      mnesia:write(Node)
+    end
+  ).
+
+deleteNode(NodeId) when is_integer(NodeId) ->
+  mnesia:activity(async_dirty,
+  fun() ->
+    mnesia:delete({node, NodeId})
+  end
+).
+
+updateNode(Id, {NewX, NewY}) when is_integer(NewX) andalso is_integer(NewY) ->
+  mnesia:activity(async_dirty,
+    fun() ->
+      [N] = mnesia:read(node, Id),
+      mnesia:write(N#node{posX = NewX, posY = NewY})
+    end
+  );
+% @ToDo: don't know what kind of guard should be used here...
+updateNode(Id, Text) ->%when is_bitstring(Text) ->
+  mnesia:activity(async_dirty,
+    fun() ->
+      [N] = mnesia:read(node, Id),
+      mnesia:write(N#node{text = Text})
+    end
+  ).
+
+getAllNodes() ->
+  mnesia:activity(async_dirty,
+    fun() ->
+      mnesia:select(node, ets:fun2ms(fun(N) -> N end))
+    end
+  ).
+
+getNodeById(Id) when is_integer(Id) ->
+  mnesia:activity(async_dirty,
+    fun() ->
+      [N] = mnesia:read(node, Id),
+      N
+    end
+  ).
+
+
+
+updateFFID(NewValue) when is_integer(NewValue) ->
+  mnesia:activity(async_dirty,
+    fun() ->
+      mnesia:write(#config{key = firstFreeId, val = NewValue})
+    end
+  ).
+
+getOrCreateFFID(NewValue) when is_integer(NewValue) ->
+  mnesia:activity(async_dirty,
+    fun() ->
+      case mnesia:read({config, firstFreeId}) of
+        [] ->
+          mnesia:write(#config{key = firstFreeId, val = NewValue}),
+          NewValue;
+        [#config{val = Val}] -> Val;
+        _ ->
+          error_msg("Problem with firstFreeIds. (multiple present?)")
+      end
+    end
+  ).
